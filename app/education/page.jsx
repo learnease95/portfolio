672efdb9a55885b8bc84a5de4c0c726ffdb765ea
learnease95 <learnease95@gmail.com>
2024-06-18 12:39:06 -0700

    first commit for education page

--- app/education/page.jsx
+++ app/education/page.jsx
@@ -1,7 +1,32 @@
+import styles from "./page.module.css";
+
 export default function Education() {
   return (
-    <div>
-      <h3>Education page</h3>
+    <div className={styles.container}>
+      <h4 className={`${styles.title} ${styles.slide_1}`}>
+        Qualification details
+      </h4>
+      <div
+        className={` ${styles.qualification_details_container} ${styles.slide_2}`}
+      >
+        <div className={styles.item}>
+          <h5 className={styles.hi}>BACHELOR OF COMPUTER APPLICATIONS</h5>
+          <p>
+            Assam Don Bosco University
+            <br />
+            Year: 2019 - 2022
+          </p>
+        </div>
+        <div className={styles.hr} />
+        <div className={styles.item}>
+          <h5 className={styles.hi}>MASTER OF COMPUTER APPLICATIONS</h5>
+          <p>
+            Assam Don Bosco University
+            <br />
+            Year: 2022 - 2024
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
